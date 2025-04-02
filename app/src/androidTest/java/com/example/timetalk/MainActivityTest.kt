b311cc759ffe164a5f0da08e078cbc69228d4ba8
jhpark <hidragon79@gmail.com>
2025-04-02 16:31:35 +0900

    임포트 파일들 변경됨.

--- app/src/androidTest/java/com/example/timetalk/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/timetalk/MainActivityTest.kt
@@ -1,17 +1,18 @@
 package com.example.timetalk
 
 import android.speech.tts.TextToSpeech
-import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers.anyOf
-import org.junit.Test
-import org.junit.runner.RunWith
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
