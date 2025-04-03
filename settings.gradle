978246c427f48cbc3a176e4ae206aec1b65e752f
jhpark <hidragon79@gmail.com>
2025-04-03 14:22:09 +0900

    오류가 계속되고 있네요. Gradle 7.0 이상에서는 저장소 설정 방식이 많이 바뀌었습니다. 전체 설정을 처음부터 다시 정리해보겠습니다:
    1. 먼저 settings.gradle 파일을 수정합니다:
    
    ```gradle:settings.gradle
    pluginManagement {
        repositories {
            google()
            mavenCentral()
            gradlePluginPortal()
        }
    }
    
    dependencyResolutionManagement {
        repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
        repositories {
            google()
            mavenCentral()
        }
    }
    
    rootProject.name = "timetalk"
    include ':app'
    ```
    
    2. build.gradle 파일을 수정합니다:
    
    ```gradle:build.gradle
    // Top-level build file
    plugins {
        id 'com.android.application' version '8.2.2' apply false
        id 'com.android.library' version '8.2.2' apply false
        id 'org.jetbrains.kotlin.android' version '1.9.22' apply false
    }
    ```
    
    3. app/build.gradle 파일은 그대로 유지합니다.
    
    4. gradle-wrapper.properties 파일도 확인합니다:
    
    ```properties:gradle/wrapper/gradle-wrapper.properties
    distributionUrl=https\://services.gradle.org/distributions/gradle-8.2-bin.zip
    ```
    
    이러한 변경사항들은:
    1. 새로운 Gradle 플러그인 관리 방식을 사용합니다.
    2. buildscript 블록 대신 plugins 블록을 사용합니다.
    3. 저장소 설정을 settings.gradle에 집중시킵니다.
    
    이제 다음 명령어로 다시 시도해보세요:
    
    ```bash
    ./gradlew clean
    ./gradlew build --refresh-dependencies
    ```
    
    이 방식이 Gradle 7.0 이상의 권장되는 설정 방식입니다.

--- settings.gradle
+++ settings.gradle
@@ -1,13 +1,13 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
